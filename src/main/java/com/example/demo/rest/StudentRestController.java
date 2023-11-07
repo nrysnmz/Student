c4666497268a4ceca3abbc8dce2abe1e09885b4f
Nuray Korganci <nrysnmz@gmail.com>
2023-11-07 10:19:29 -0600

    m

--- src/main/java/com/example/demo/rest/StudentRestController.java
+++ src/main/java/com/example/demo/rest/StudentRestController.java
@@ -5,21 +5,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
+    private List<Student> theStudents;
 
-   // define endpoint for "/studnets" -return a list of students
+    //define @PostConstruct to laod the student data... only once!
+    @PostConstruct
+    public void loadData() {
+        theStudents = new ArrayList<>();
 
+        theStudents.add(new Student("John", "Paul"));
+        theStudents.add(new Student("Mario", "Nail"));
+        theStudents.add(new Student("Peter", "Pan"));
+    }
+
+
+    // define endpoint for "/studnets" -return a list of students
     @GetMapping("/students")
-    public List<Student> getStudents(){
-        List<Student> theStudents = new ArrayList<>();
-        theStudents.add(new Student("John", "Paul" ));
-        theStudents.add(new Student("Mario", "Nail" ));
-        theStudents.add(new Student("Peter", "Pan" ));
+    public List<Student> getStudents() {
+
         return theStudents;
     }
 
