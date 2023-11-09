9372f675c421927150951e64045d08c471ff85c4
Nuray Korganci <nrysnmz@gmail.com>
2023-11-09 11:43:26 -0600

    StudentRestExceptionHandler added.

--- src/main/java/com/example/demo/rest/StudentRestController.java
+++ src/main/java/com/example/demo/rest/StudentRestController.java
@@ -42,37 +42,8 @@ public class StudentRestController {
         }
         return theStudents.get(studentId);
     }
-    //Add an exception handler using @ExceptionHandler
-
-    @ExceptionHandler
-
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFundException exception) {
-
-        //create a StudentErrorResponse
-
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(exception.getMessage());
-        error.setTimestamp(System.currentTimeMillis());
-
-        //return a ResponseEntity.
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
-    // add another exception handler... to catch any exception(catch all)
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(Exception exception) {
-
-        //create a StudentErrorResponse
-
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMessage(exception.getMessage());
-        error.setTimestamp(System.currentTimeMillis());
-
-        //return a ResponseEntity.
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
-    }
 }
+
+
+
+
